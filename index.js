616dedd5ef7a46db15022dc9656a06b313f1f68b
rcslbaa <ifasmarani3125@gmail.com>
2025-10-20 13:50:22 +0700

    menghapus data mahasiswa

--- index.js
+++ index.js
@@ -76,3 +76,13 @@ app.put('/api/users/:id', (req, res) => {
     )
 })
 
+app.delete('/api/users/:id', (req, res) => {
+    const userId = req.params.id;
+    db.query('DELETE FROM mahasiswa WHERE id = ?', [userId], (err, results) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).json({ message: 'Database error' });
+        }
+        res.json({ message: 'User deleted successfully' });
+    });
+});
